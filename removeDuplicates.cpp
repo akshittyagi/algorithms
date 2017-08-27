58516e6dacb21251dfcb24a1a1507e6489f4b5d2
Akshit Tyagi <akty.tyagi7@gmail.com>
2017-08-27 22:56:51 +0530

    lint

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -8,17 +8,19 @@ Given input array nums = [1,1,2],
 
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 */
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
+class Solution
+{
+  public:
+    int removeDuplicates(vector<int> &nums)
+    {
         int initLength = nums.size();
         int i = 0;
         int actualLength = 0;
         int start = 1;
-        while(i<nums.size())
-        {   
+        while (i < nums.size())
+        {
             int j = i;
-            while(nums[j]==nums[i])
+            while (nums[j] == nums[i])
                 j++;
             i = j;
             nums[start] = nums[i];

0cafbb8d4496424c4acb9d828844ad5e29463e99
Akshit Tyagi <akty.tyagi7@gmail.com>
2017-09-01 20:20:24 +0530

    Added mergeKSortedLinkedLists

--- mergeKSorted.cpp
+++ mergeKSorted.cpp
@@ -1,45 +1,62 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+/**
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-struct ListNode
+struct Comparator
 {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    bool operator()(const ListNode *node1, const ListNode *node2)
+    {
+        return node1->val > node2->val;
+    }
 };
 class Solution
 {
   public:
-    ListNode *mergeKLists(std::vector<ListNode *> &lists)
+    ListNode *mergeKLists(vector<ListNode *> &lists)
     {
-        std::priority_queue<int> pq;
-        //init the heap with the first elements of the k-sorted lists
-        for (int i = 0;i<lists.size();i++)
+
+        if (lists.size() == 0)
+            return NULL;
+        priority_queue<ListNode *, vector<ListNode *>, Comparator> pq;
+        for (auto &it : lists)
+            if (it != NULL)
+                pq.push(it);
+
+        ListNode *ret = new ListNode(INT_MIN);
+        ListNode *head = new ListNode(INT_MIN);
+
+        while (!pq.empty())
         {
-            ListNode *head = lists[i];
-            printf("Pushing: %d\n", head->val);
-            pq.push(head->val);
+
+            ListNode *minElem = pq.top();
+            pq.pop();
+            if (ret->val == INT_MIN)
+            {
+                head = new ListNode(minElem->val);
+                ret = head;
+            }
+            else
+            {
+                ret->next = new ListNode(minElem->val);
+                ret = ret->next;
+            }
+            if (minElem->next != NULL)
+                pq.push(minElem->next);
+            else
+                continue;
         }
 
-        ListNode *ret = new ListNode(5);
-        return ret;
+        if (ret->val == INT_MIN)
+            return NULL;
+        else
+            ret->next = NULL;
+        return head;
     }
 };
-
-int main()
-{
-    int K = 5;
-    std::vector<ListNode *> lists(5);
-    for (int i = 0; i < 5; i++)
-    {
-        int start = i + 1;
-        ListNode *head = new ListNode(start);
-        printf("%d\n", head->val);
-        lists.push_back(head);
-    }
-    Solution solution;
-    ListNode *ret = solution.mergeKLists(lists);
-}

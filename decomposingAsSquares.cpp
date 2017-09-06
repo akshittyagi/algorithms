4d792570c3b117fdb053dbf8678142e7f2f8f96a
Akshit Tyagi <akty.tyagi7@gmail.com>
2017-09-06 20:46:01 +0530

    Added dp problems

---
+++ decomposingAsSquares.cpp
@@ -0,0 +1,23 @@
+/**
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+*/
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n==0)
+            return 0;
+        vector<int> dp(n+1, 0);
+        for(int i=0;i<=n;i++)
+        {
+            dp[i] = i;
+            for(int j=2;j<=sqrt(i);j++)
+            {
+                dp[i] = min(dp[i], 1 + dp[i-j*j]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
---
+++ maximalSquare.cpp
@@ -0,0 +1,47 @@
+/**
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 4.
+s
+*/
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        if(m==0)
+            return 0;
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int maxSize = 0;
+        for(int j=0;j<n;j++)
+        {
+            dp[0][j] = matrix[0][j] - '0';
+            maxSize = max(maxSize, dp[0][j]);
+        }
+        for(int i=0;i<m;i++)
+        {
+            dp[i][0] = matrix[i][0] - '0';
+            maxSize = max(maxSize, dp[i][0]);
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]=='1')
+                {
+                    dp[i][j] = min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]) ) + 1;
+                    maxSize = max(maxSize, dp[i][j]);
+                }
+            }
+        }
+        
+        return maxSize*maxSize;
+    }
+};

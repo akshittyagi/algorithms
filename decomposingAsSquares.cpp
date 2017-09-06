2f445c32358783b27939b59d869ef50d0c3966b1
Akshit Tyagi <akty.tyagi7@gmail.com>
2017-09-06 20:53:20 +0530

    format lint

--- decomposingAsSquares.cpp
+++ decomposingAsSquares.cpp
@@ -3,21 +3,23 @@ Given a positive integer n, find the least number of perfect square numbers (for
 
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 */
-class Solution {
-public:
-    int numSquares(int n) {
-        if(n==0)
+class Solution
+{
+  public:
+    int numSquares(int n)
+    {
+        if (n == 0)
             return 0;
-        vector<int> dp(n+1, 0);
-        for(int i=0;i<=n;i++)
+        vector<int> dp(n + 1, 0);
+        for (int i = 0; i <= n; i++)
         {
             dp[i] = i;
-            for(int j=2;j<=sqrt(i);j++)
+            for (int j = 2; j <= sqrt(i); j++)
             {
-                dp[i] = min(dp[i], 1 + dp[i-j*j]);
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
             }
         }
-        
+
         return dp[n];
     }
 };
